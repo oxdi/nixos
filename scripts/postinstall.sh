00f616218797e19b52a08e6b8f31f187df8c4a3f
chrisfarms <chris@oxdi.eu>
2014-01-31 17:36:18 +0000

    use 14.02 as we need postgres93

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -3,8 +3,6 @@
 set -e
 
 # Update and initialize our new configuration
-nix-channel --remove nixos
-nix-channel --add http://nixos.org/channels/nixos-unstable nixos
 nixos-rebuild switch --upgrade
 
 # Cleanup any previous generations and delete old packa
