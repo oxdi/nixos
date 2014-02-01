27993a5f78682b605ca80b28cf3fd2d471e9aa96
Chris Farmiloe <chrisfarms@gmail.com>
2014-02-01 20:04:09 +0000

    use custom nixpkgs and fix virtualbox

--- nixos/sdk.nix
+++
@@ -1,5 +0,0 @@
-{ config, pkgs, ... }:
-
-{
-	# add sdk configure here
-}
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -2,11 +2,9 @@
 
 set -e
 
-# Switch to unstable channel
+# rebuild (which should now use our nixpkgs)
 nix-channel --remove nixos
 nix-channel --add http://nixos.org/channels/nixos-unstable nixos
-
-# Update and initialize our new configuration
 nixos-rebuild switch --upgrade
 
 # Cleanup any previous generations and delete old packa
