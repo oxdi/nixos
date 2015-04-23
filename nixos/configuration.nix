a74dc912ea34c07254bc2f00e3f107da7a020237
Ben Smith <polynomial@gmail.com>
2015-04-23 12:06:22 -0700

    NIXOS 15 Packer Setup
    Just pushing up changes to show how this works

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -4,13 +4,11 @@
   imports = [ 
     ./hardware-configuration.nix 
     ./guest.nix
-    ./users.nix
-    ./vagrant.nix
   ];
 
+  users.mutableUsers = false;
   # we always want git and vim
   environment.systemPackages = with pkgs; [ 
-    git
     vim
   ];
 
